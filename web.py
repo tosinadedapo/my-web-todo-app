149bd4914f682274038c21dfc70f8724beca7331
tosinadedapo <tosinoreka@yahoo.com>
2024-03-08 14:52:55 +0100

    Input set on top

--- web.py
+++ web.py
@@ -15,6 +15,9 @@ st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
 
+st.text_input(label="", placeholder="Add a new todo...",
+              on_change=add_todo, key='new_todo')
+
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
@@ -22,8 +25,5 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-
-st.text_input(label="", placeholder="Add a new todo...",
-              on_change=add_todo, key='new_todo')
 
 st.session_state
