fa368586667bca3219b8e5ad27a24a66478056a6
tosinadedapo <tosinoreka@yahoo.com>
2024-03-08 15:40:21 +0100

    Introduced html to st.write

--- web.py
+++ web.py
@@ -13,7 +13,8 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b> productivity </b>",
+         unsafe_allow_html=True)
 
 st.text_input(label="", placeholder="Add a new todo...",
               on_change=add_todo, key='new_todo')
